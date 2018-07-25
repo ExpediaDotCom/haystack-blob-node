ad820d7846a7b1c27662d350cde3cb6a70d65e35
Ashish Aggarwal <asaggarwal@DELC02T84M5G8WN.sea.corp.expecn.com>
2018-07-25 18:06:17 +0530

    fixing index.d.ts declarations

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,11 @@
 import Configuration from './config';
 import {BlobContentType} from './blob_content_type';
 
+export {
+ Configuration,
+ BlobContentType
+};
+
 module.exports = {
     initBlobClient: Configuration.initBlobClient,
     blobContentType: BlobContentType
